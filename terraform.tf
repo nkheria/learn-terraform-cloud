12bca05bfa1a78cc2a21c6bd8c90e9d7ed3dfac6
root <root@terraform-new.us-central1-a.c.kinetic-dogfish-366710.internal>
2022-11-24 06:13:02 +0000

    removed cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "demo-app-2022"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
